80c051ce814002f6c928432497a91bb64642939c
gabe_pi_server <27024731+fynecontry@users.noreply.github.com>
2022-09-11 09:19:15 +0100

    Refactor img_downloader.py to use BeautifulSoup

--- image_site_downloader/img_downloader.py
+++ image_site_downloader/img_downloader.py
@@ -2,30 +2,51 @@
 # img_downloader.py     - searches flickr for CLI provided word and 
 #                       downloads 10 images into a folder
 
-import sys, os, logging, requests
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+import sys, os, logging, requests,bs4
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='img_downloader.log',level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# keyword obtained from CLI
-if len(sys.argv) < 2:
-    print('usage: img_downloader.py [search_keyword]')
-    sys.exit(1)
+def get_keyword():
+    # keyword obtained from CLI
+    if len(sys.argv) < 2:
+        print('usage: img_downloader.py [search_keyword]')
+        sys.exit(1)
+    else:
+        search_keyword = ' '.join(sys.argv[1:])
+    return search_keyword
 
-else:
-    search_keyword = ' '.join(sys.argv[1:])
+def download_webpage(search_keyword):
+    '''Search flickr using keyword and dowloads webpage'''
+    url = 'https://www.flickr.com/search/?text=' + search_keyword
+    # Download the page
+    try:
+        logging.info('Downloading page')
+        res = requests.get(url)
+        res.raise_for_status()
+        logging.info('Downloaded page successfully')
+    except Exception as err:
+        print(f'There was a problem fetching page: {err}')
+        logging.critical(f'Fetch {url} request failed: {err}')
+        sys.exit(1)
+    return res
 
-# Open browser and redirect to flickr
-browser = webdriver.Chrome()
-browser.get('https://www.flickr.com/')
+def download_images(page_response):
+    '''Retrive list of image elements from webpage reponse and save to disk'''
+    page_soup = bs4.BeautifulSoup(page_response.text, 'html.parser')
+    img_elems = page_soup.select('.photo-list-pjdkhoto-container')
+    if img_elems == []:
+        print('Could not find images')
+        logging.warning('Empty img_elems list, page_soup could not find images using class: .photo-list-photo-container')
 
 
-# TODO: Search site using search_keyword
+def main():
+    # Get search word(s) from user
+    search_keyword = get_keyword()
 
+    #search and download result
+    page_response = download_webpage(search_keyword)
 
-# TODO: Download # of images
+    download_images(page_response)
 
-
-# TODO: Write images to folder 
+if __name__ == "__main__":
+    main()
